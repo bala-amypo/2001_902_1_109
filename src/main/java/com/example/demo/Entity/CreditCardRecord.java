341eca6a9cae15ab5619efa631bb04c40a2396e8
bala-amypo <bala@amypo.in>
2025-12-19 08:19:07 +0000

    User commit

--- src/main/java/com/example/demo/Entity/CreditCardRecord.java
+++ src/main/java/com/example/demo/Entity/CreditCardRecord.java
@@ -23,5 +23,24 @@ public class CreditCardRecord{
     public String getUserId(){
         return userId;
     }
+    public void setUserId(String userId){
+        this.userId=userId;
+    }
+    public String getFullName(){
+        return fullName;
+    }
+    public void setFullName(String fullName){
+        this.fullName=fullName;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void 
     
 }
