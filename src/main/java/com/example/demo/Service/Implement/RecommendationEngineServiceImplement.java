715310a56c3c011ae053ef4c3d417d2e0fcd63d5
bala-amypo <bala@amypo.in>
2025-12-21 09:35:29 +0000

    User commit

--- src/main/java/com/example/demo/Service/Implement/RecommendationEngineServiceImplement.java
+++ src/main/java/com/example/demo/Service/Implement/RecommendationEngineServiceImplement.java
@@ -2,13 +2,13 @@ package com.example.demo.Service.Implement;
 
 import com.example.demo.Entity.*;
 import com.example.demo.Repository.*;
-import com.example.demo.Service.RecommendationService;
+import com.example.demo.Service.RecommendationEngineService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class RecommendationServiceImpl implements RecommendationService {
+public class RecommendationServiceImpl implements RecommendationEngineService {
 
     private final PurchaseIntentRecordRepository intentRepository;
     private final CreditCardRecordRepository cardRepository;
