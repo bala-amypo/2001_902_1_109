3e426452d7cc5f41b6321c68f3b6b48bafbfba19
bala-amypo <bala@amypo.in>
2025-12-21 10:09:00 +0000

    User commit

---
+++ src/main/java/com/example/demo/Service/Implement/RecommendationEngineServiceImplement.java
@@ -0,0 +1,28 @@
+package com.example.demo.Service.Implement;
+
+import com.example.demo.entity.RecommendationRecord;
+import com.example.demo.Repository.RecommendationRepository;
+import com.example.demo.Service.RecommendationEngineService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RecommendationEngineServiceImplement implements RecommendationEngineService {
+
+    private final RecommendationRepository repository;
+
+    public RecommendationEngineServiceImplement(RecommendationRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public RecommendationRecord saveRecommendation(RecommendationRecord recommendation) {
+        return repository.save(recommendation);
+    }
+
+    @Override
+    public List<RecommendationRecord> getAllRecommendations() {
+        return repository.findAll();
+    }
+}
